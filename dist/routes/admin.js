0f2fc4ce03629fb58f52aedeb1bbc7c596347435
RODRIGO ALFREDO DEL CARMEN HERNANDEZ <radel220499@gmail.com>
2025-10-14 09:23:23 -0600

    probando backend

--- dist/routes/admin.js
+++ dist/routes/admin.js
@@ -21,6 +21,10 @@ router.get("/api/admin/psicologos", validarAdmin_1.default, admin_1.getAllPsicol
 router.put("/api/admin/psicologos/:id_psicologo/status", validarAdmin_1.default, admin_1.cambiarStatusPsicologo);
 router.delete("/api/admin/psicologos/:id_psicologo", validarAdmin_1.default, admin_1.eliminarPsicologo);
 router.post("/api/admin/psicologos/:id_psicologo/validar-cedula-api", validarAdmin_1.default, admin_1.validarCedulaConAPI);
+//  Validación manual de cédula
+router.put("/api/admin/psicologos/:id_psicologo/validar-cedula-manual", validarAdmin_1.default, admin_1.validarCedulaManual);
 // Gestión de pacientes
-router.get("/api/admin/pacientes", validarAdmin_1.default, admin_1.getAllPacientes);
+router.get("/api/admin/pacientes", validarAdmin_1.default, admin_1.getAllPacientesAdmin);
+router.put("/api/admin/pacientes/:id_paciente/reasignar", validarAdmin_1.default, admin_1.reasignarPaciente);
+router.put("/api/admin/pacientes/:id_paciente/status", validarAdmin_1.default, admin_1.cambiarEstadoPaciente);
 exports.default = router;
